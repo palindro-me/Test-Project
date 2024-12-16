dec3c481466b01842351d0a83e9aa11b8f66b3b8
Anna Kazak <anna.s.kazak@gmail.com>
2024-12-16 18:42:59 +0100

    header

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Logo = () => {
+    return (
+        <div>
+            <a href="">
+                <img src="" alt=""/>
+            </a>
+        </div>
+    );
+};
+
